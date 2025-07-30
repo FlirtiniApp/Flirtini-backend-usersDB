fb365c53cf934afc6871549b99b83a2b86bec47e
Wiktor Ilczuk <137764780+Wilczuk1506@users.noreply.github.com>
2025-07-30 11:28:22 +0200

    update api

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const User = require('./models/user.model'); 
 
-
 // Connect to the database and start the server
 console.log("Connecting to database...");
 mongoose.connect(`mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@flirtini.5tabe6e.mongodb.net/Flirtini?retryWrites=true&w=majority&appName=Flirtini`)
     .then(() => {
-        console.log("Connected to database");
+        console.log("\x1b[35mConnected to database\x1b[0m");
         app.listen(port, () => {
-            console.log(`App listening on port ${port}.`);
+            console.log(`\x1b[34mApp listening on port ${port}.\x1b[0m`);
         });
     })
     .catch((err) => {
-        console.log("Failed to connect to database");
+        console.log("\x1b[41mFailed to connect to database\x1b[0m");
         console.log(err);
     });
 
@@ -32,13 +31,32 @@ async function createUserHandler(req, res) {
     }
 }
 
-// Handler for getting all users
+// Handler for getting users
 async function getUserHandler(req, res) {
     try {
         const {id} = req.params;
 
         const users = id === "all" ? await User.find({}) : await User.findById(id);
+
         res.status(200).json(users);
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+// Handler for updating a user
+async function updateUserHandler(req, res) {
+    try {
+        const {id} = req.params;
+
+        const newUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+
+        if (!newUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(newUser);
     }
     catch (error) {
         res.status(500).json({ message: error.message });
@@ -50,3 +68,6 @@ app.post('/users/create', createUserHandler);
             
 // All users retrieval route
 app.get('/users/:id', getUserHandler);
+
+// User update route
+app.put('/users/:id', updateUserHandler);
